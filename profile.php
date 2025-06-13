1ab2fbcd104cafe22e3cd7624aa9f483a53872e7
Elton-777 <145193852+Elton-777@users.noreply.github.com>
2025-06-13 12:31:11 +0200

    gjyh

---
+++ profile.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Peofile | My book</title>
+    </head>
+
+    <style type="text/css">
+        #blue_bar{
+            height: 50px;
+            background-color: #405d9b;
+            color: #d9dfeb;
+        }
+
+
+        #search_box{
+            width: 400px;
+            height: 20px;
+            border-radius: 5px;
+            border:none;
+            padding: 4px;
+            font-size: 14px;
+            background-image: url(search.png);
+            background-repeat: no-repeat;
+            background-position: right;
+
+        }
+</style>
+
+    <body style="font-family: tahoma; background-color: #d0d8e4;">
+        <br>
+
+    <!-- top bar-->
+
+    <div id="blue_bar">
+        <div style="Width: 800px; margin:auto; font-size: 30px;">
+            Mybook &nbsp  &nbsp<input type="text" id="search_box" placeholder="Search for people">
+            <img src="OIP.jfif" style="width: 55px; float: right;">
+        </div>
+    </div>
+
+    <div style="width: 800px; margin:auto; background-color: black; min-height: 550px;">
+
+    <div style="background-color: white; text-align: center; color: #405d9b;">
+        <img src="OIP (1).jfif" style="width: 600px;">
+        <img id="profile_pic" src="selfie.jfif">
+        <br>
+        Timeline About Friends Photos Settings
+    </div>
+
+    </div>
+
+    </body>
+</html>
