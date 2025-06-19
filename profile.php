f2bee161d9ee0f2f0be56169061608ed1521a9e2
Elton-777 <145193852+Elton-777@users.noreply.github.com>
2025-06-19 13:13:10 +0200

    kuy

--- profile.php
+++ profile.php
@@ -1,53 +1,136 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Peofile | My book</title>
+        <title>Profile | My book</title>
     </head>
 
     <style type="text/css">
         #blue_bar{
-            height: 50px;
+            height:50px;
             background-color: #405d9b;
             color: #d9dfeb;
         }
-
 
         #search_box{
             width: 400px;
             height: 20px;
             border-radius: 5px;
-            border:none;
+            border: none;
             padding: 4px;
             font-size: 14px;
-            background-image: url(search.png);
+            background-image: url( search.jfif);
             background-repeat: no-repeat;
             background-position: right;
+        }
+
+        #profile_pic{
+            width: 250px;
+            margin-top: -250px;
+            border-radius: 50%;
+            border: solid 2px white;
+
 
         }
-</style>
 
-    <body style="font-family: tahoma; background-color: #d0d8e4;">
+        #menu_buttons{
+            width: 100px;
+            display: inline-block;
+            margin: 6px;
+            
+        }
+
+        #friends_img{
+            width: 75px;
+            float: left;
+            margin: 8px;
+
+        }
+
+        #friends_bar{
+            background-color: white;
+            min-height: 400px;
+            margin-top: 20px;
+            color: #aaa;
+            padding: 8px;
+        }
+
+        #friends{
+            clear: both;
+        }
+        
+
+    </style>
+    <body  style="font-family: tahoma; background-color: #d0d8e4">
         <br>
+        <div id="blue_bar">
+            <div style="width: 800px;margin: auto;font-size: 30px;">
 
-    <!-- top bar-->
+            Mybook  &nbsp<input type="text" id="search_box" placeholder="Search for people">
+            <img src="selfie.jfif" style="width: 88px; float: right;">
 
-    <div id="blue_bar">
-        <div style="Width: 800px; margin:auto; font-size: 30px;">
-            Mybook &nbsp  &nbsp<input type="text" id="search_box" placeholder="Search for people">
-            <img src="OIP.jfif" style="width: 55px; float: right;">
+
+            </div>
+
+       </div>
+       <div style="width: 700px; margin: auto; min-height: 400px;">
+        <div style="background-color: white; text-align: center; color: #405d9b">
+            <img src="mountain.jfif" style="width:100%;">
+            <img id="profile_pic" src="selfie2.jpg"><br>
+            <div style=" font-size:20px;">Mary Banda</div>
+            
+            <br>
+            <div id="menu_buttons">Timeline</div> 
+            <div id="menu_buttons">About</div>
+             <div id="menu_buttons">Friends</div>
+             <div id="menu_buttons">Photos</div>
+             <div id="menu_buttons">Settigns</div>
+        </div>
+
+        <div style="display: flex;">
+
+        <div style="min-height:400px; flex:1;">
+            <div id="friends_bar">
+                Friends<br>
+
+                <div id="friends">
+                    <img id="friends_img" src="guy.jfif">
+                    <br>
+                    First user
+                </div>
+
+                <div id="friends">
+                    <img id="friends_img" src="guy.jfif">
+                    <br>
+                    First user
+                </div>
+
+                <div id="friends">
+                    <img id="friends_img" src="guy.jfif">
+                    <br>
+                    First user
+                </div>
+
+                <div id="friends">
+                    <img id="friends_img" src="guy.jfif">
+                    <br>
+                    First user
+                </div>
+                
+
+
+            </div>
+            
+
+        </div> 
+
+        <div style="min-height: 400px; flex:2.5;">
+
         </div>
     </div>
 
-    <div style="width: 800px; margin:auto; background-color: black; min-height: 550px;">
 
-    <div style="background-color: white; text-align: center; color: #405d9b;">
-        <img src="OIP (1).jfif" style="width: 600px;">
-        <img id="profile_pic" src="selfie.jfif">
-        <br>
-        Timeline About Friends Photos Settings
-    </div>
 
-    </div>
+       </div>
 
     </body>
 </html>
